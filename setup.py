4fd0bcdd588f744bf4719704a6deec44235e470c
John Mertic <jmertic@linuxfoundation.org>
2019-05-01 16:43:46 -0400

    Add pip install support
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='dco-org-check',
+     version='0.1',
+     scripts=['dco-org-check'] ,
+     author="John Mertic",
+     author_email="jmertic@gmail.com",
+     description="Script to check a GitHub org for commits without a DCO signoff that should have one.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/jmertic/dco-org-check",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: Apache Software License",
+         "Operating System :: OS Independent",
+     ],
+ )
