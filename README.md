bbe44729de6655675dcb0905f654fad03b47a408
John Mertic <jmertic@linuxfoundation.org>
2019-05-01 10:56:23 -0400

    Feature for building out a prior commits file for each committter/repo.
    Add better error message if config file doesn't exist.
    
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Loads config file ( dco_org_check.yaml ) for credentials
 * token - GitHub access token
 * org - Github org name
 * csvfile - name of csvfile ( defaults to dco_issues.csv )
+* create_prior_commits_file - if 1, creates out a prior commits signoff file for each committer and repo for them to check in.
 
 SPDX-License-Identifier: Apache-2.0
--- dco-org-check.py
+++ dco-org-check.py
@@ -16,12 +16,17 @@ import re
 import csv
 import yaml
 import io
+import os
 
 from github import Github
 
 def loadconfig():
-    with open("dco_org_check.yaml", 'r') as stream:
-        data_loaded = yaml.safe_load(stream)
+    try:
+        with open("dco_org_check.yaml", 'r') as stream:
+            data_loaded = yaml.safe_load(stream)
+    except:
+        sys.exit("dco_org_check.yaml config file is not defined")
+
 
     if not data_loaded['token']:
         raise Exception('\'token\' is not defined')
@@ -29,6 +34,8 @@ def loadconfig():
         raise Exception('\'org\' is not defined')
     if not data_loaded['csvfile']:
         data_loaded['csvfile'] = "dco_issues.csv"
+    if not data_loaded['create_prior_commits_file']:
+        data_loaded['create_prior_commits_file'] = 0
 
     return data_loaded
 
@@ -58,3 +65,19 @@ for repo in g.get_organization(config['org']).get_repos():
         except GithubException as e:
             if e.status == 502:
                  csv_writer.writerow([commit.commit.html_url,commit.commit.message,commit.commit.author.name,commit.commit.author.email,commit.commit.author.date])
+
+        if config['create_prior_commits_file']:
+            username = commit.commit.author.name
+            url_search = re.search("https://github.com/"+config['org']+"/(.*)/commit/(.*)",commit.commit.html_url)
+            repo = url_search.group(1)
+            sha = url_search.group(2)
+            commitfilename = username+'-'+repo+'.txt'
+
+            if not os.path.isfile(commitfilename):
+                fh = open(commitfilename,  mode='w+')
+                fh.write("The following commits were made pursuant to the Developer Certificate of Origin, even though a Signed-off-by: was not included in the commit message.\n\n")
+            else:
+                fh = open(commitfilename,  mode='a')
+
+            fh.write(sha+" "+commit.commit.message+"\n")
+            fh.close()
